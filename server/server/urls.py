0c2f0434f6cc249986bf3a2043d8261bb7c9b1e9
Liang Sun <i@liangsun.org>
2015-11-12 21:06:00 +0800

    Add test view.

--- server/server/urls.py
+++ server/server/urls.py
@@ -17,5 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^$', include('app.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
