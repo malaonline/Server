096d87f5b86e0f62821b67b8a3a5c86211e2ab24
Liang Sun <i@liangsun.org>
2015-11-17 20:20:35 +0800

    Initlize grade data.

---
+++ server/app/migrations/0004_grade.py
@@ -0,0 +1,42 @@
+import os
+
+from django.db import migrations
+
+def add_grade(apps, schema_editor):
+    Grade = apps.get_model('app', 'Grade')
+
+    p = Grade(name='小学',leaf=False)
+    p.save()
+    for g in ('一', '二', '三', '四', '五', '六'):
+        name = '%s年级' % g
+        t = Grade(name=name, superset=p, leaf=True)
+        t.save()
+        print(t.name)
+
+    p = Grade(name='初中',leaf=False)
+    p.save()
+    for g in ('一', '二', '三', '四'):
+        name = '初%s' % g
+        t = Grade(name=name, superset=p, leaf=True)
+        t.save()
+        print(t.name)
+
+    p = Grade(name='高中',leaf=False)
+    p.save()
+    for g in ('一', '二', '三'):
+        name = '高%s' % g
+        t = Grade(name=name, superset=p, leaf=True)
+        t.save()
+        print(t.name)
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('app', '0001_initial'),
+        ('app', '0002_subject'),
+        ('app', '0003_region'),
+    ]
+
+    operations = [
+        migrations.RunPython(add_grade),
+    ]
