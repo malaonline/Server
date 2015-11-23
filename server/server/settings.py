8f63beca15aaff403c220f159c8e8b366c327ba4
Leon S <i@leons.im>
2015-11-23 11:31:12 +0800

    Add static root.

--- server/server/settings.py
+++ server/server/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/var/www/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'upload')
 
 try:
