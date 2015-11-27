6c0012c3a379c5196f08020511b4e1d98f474558
jackyZhang <15957031@qq.com>
2015-11-27 19:45:01 +0800

    remove no use

--- android/app/src/main/java/com/malalaoshi/android/fragments/TeacherFragment.java
+++ android/app/src/main/java/com/malalaoshi/android/fragments/TeacherFragment.java
@@ -50,7 +50,7 @@ public class TeacherFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_teacher_list2, container, false);
+        View view = inflater.inflate(R.layout.fragment_teacher_list, container, false);
 
         // Set the adapter
         if (view instanceof RecyclerView) {
