325b8600a9ef60f595ea386f40237ba372c89d7d
Leon <i@leons.im>
2015-11-27 11:04:17 +0800

    Add teachers and teacher api.

--- server/server/urls.py
+++ server/server/urls.py
@@ -20,6 +20,8 @@ from django.contrib.auth.models import User
 from rest_framework import routers, serializers, viewsets
 from rest_framework.authtoken import views
 
+from app.models import Teacher
+
 # Serializers define the API representation.
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
@@ -31,9 +33,19 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+class TeacherSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Teacher
+        fields = ('url', 'name', 'degree', 'active', 'fulltime', )
+
+class TeacherViewSet(viewsets.ModelViewSet):
+    queryset = Teacher.objects.all()
+    serializer_class = TeacherSerializer
+
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
+router.register(r'teachers', TeacherViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
