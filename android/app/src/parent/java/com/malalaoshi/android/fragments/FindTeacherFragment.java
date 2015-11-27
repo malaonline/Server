5d673e2c1413d33ae5308168552979e94b4d5eb9
Scott <zhonglijunyi@163.com>
2015-11-27 11:25:09 +0800

    find teacher condition editing fragment

--- android/app/src/parent/java/com/malalaoshi/android/fragments/FindTeacherFragment.java
+++ android/app/src/parent/java/com/malalaoshi/android/fragments/FindTeacherFragment.java
@@ -1,12 +1,17 @@
 package com.malalaoshi.android.fragments;
 
 import android.app.Activity;
-import android.app.Fragment;
 import android.net.Uri;
 import android.os.Bundle;
+import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 
 import com.malalaoshi.android.R;
@@ -14,12 +19,12 @@ import com.malalaoshi.android.R;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link MainFragment.OnFragmentInteractionListener} interface
+ * {@link FindTeacherFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link MainFragment#newInstance} factory method to
+ * Use the {@link FindTeacherFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class MainFragment extends Fragment {
+public class FindTeacherFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -28,6 +33,8 @@ public class MainFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private String[] subjectsArr;
+    private String[] gradesArr;
 
     private OnFragmentInteractionListener mListener;
 
@@ -37,11 +44,11 @@ public class MainFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment MainFragment.
+     * @return A new instance of fragment FindTeacherFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static MainFragment newInstance(String param1, String param2) {
-        MainFragment fragment = new MainFragment();
+    public static FindTeacherFragment newInstance(String param1, String param2) {
+        FindTeacherFragment fragment = new FindTeacherFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -49,7 +56,7 @@ public class MainFragment extends Fragment {
         return fragment;
     }
 
-    public MainFragment() {
+    public FindTeacherFragment() {
         // Required empty public constructor
     }
 
@@ -60,26 +67,58 @@ public class MainFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+        subjectsArr = new String[]{"语文", "数学", "英语"};
+        gradesArr = new String[]{"一年级", "二年级", "三年级", "四年级", "五年级", "六年级"};
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.fragment_main2, container, false);
-        TextView main_find_teacher_btn = (TextView)v.findViewById(R.id.main_find_teacher_btn);
-        main_find_teacher_btn.setOnClickListener(new View.OnClickListener() {
+        final View view = inflater.inflate(R.layout.fragment_find_teacher, container, false);
+        // 科目年级事件
+        View subjectsGradesRow = view.findViewById(R.id.subjects_grades_row);
+        subjectsGradesRow.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View btn) {
-                FindTeacherFragment findTeacherFragment = new FindTeacherFragment();
-                getFragmentManager()
-                        .beginTransaction()
-                        .replace(R.id.content_layout, findTeacherFragment)
-                        .addToBackStack(null)
-                        .commit();
+            public void onClick(View v) {
+                View subjectsGradesComp = view.findViewById(R.id.subjects_grades_comp);
+                int visibility = subjectsGradesComp.getVisibility();
+                if (visibility==View.GONE) {
+                    subjectsGradesComp.setVisibility(View.VISIBLE);
+                } else {
+                    subjectsGradesComp.setVisibility(View.GONE);
+                }
             }
         });
-        return v;
+        // 科目年级列表
+        ListView subjectsListView = (ListView)view.findViewById(R.id.find_teacher_subjects_list);
+        subjectsListView.setAdapter(new ArrayAdapter<String>(getActivity(), R.layout.abc_list_menu_item_layout, R.id.title, subjectsArr));
+        subjectsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                if (position<0 || position>=subjectsArr.length) {
+                    return;
+                }
+                String subject = subjectsArr[position];
+                TextView label = (TextView)view.findViewById(R.id.subject_text);
+                label.setText(subject);
+            }
+        });
+        ListView gradesListView = (ListView)view.findViewById(R.id.find_teacher_grades_list);
+        gradesListView.setAdapter(new ArrayAdapter<String>(getActivity(), R.layout.abc_list_menu_item_layout, R.id.title, gradesArr));
+        gradesListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                if (position < 0 || position >= gradesArr.length) {
+                    return;
+                }
+                String grade = gradesArr[position];
+                TextView label = (TextView) view.findViewById(R.id.grade_text);
+                label.setText(grade);
+            }
+        });
+        // 选择上课地点
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
