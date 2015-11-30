615a9f562951c77d3bb59c4e1593f20988f15608
jackyZhang <15957031@qq.com>
2015-11-30 17:35:03 +0800

    add base activity

---
+++ android/app/src/main/java/com/malalaoshi/android/base/BaseActivity.java
@@ -0,0 +1,9 @@
+package com.malalaoshi.android.base;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by zl on 15/11/30.
+ */
+public class BaseActivity extends AppCompatActivity{
+}
