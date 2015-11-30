b44895fa04675c54967c240fc2b4ace29ab89dc1
Leon <i@leons.im>
2015-11-30 18:50:39 +0800

    Refactor view sets.

--- server/app/views.py
+++ server/app/views.py
@@ -1,5 +1,105 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import serializers, viewsets
+
+from app.models import *
 
 def index(request):
     return HttpResponse("Hello, world. You're at the index.")
+
+# Serializers define the API representation.
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'email', 'is_staff', 'profile')
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class RegionSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Region
+        fields = ('url', 'name', 'superset', 'admin_level', 'leaf', 'weekly_time_slots')
+
+class RegionViewSet(viewsets.ModelViewSet):
+    queryset = Region.objects.all()
+    serializer_class = RegionSerializer
+
+class SchoolSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = School
+        fields = ('url', 'name', 'address', 'thumbnail', 'region', 'center',
+                'longitude', 'latitude',)
+
+class SchoolViewSet(viewsets.ModelViewSet):
+    queryset = School.objects.all()
+    serializer_class = SchoolSerializer
+
+class GradeSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Grade
+        fields = ('url', 'name', 'superset', 'leaf')
+
+class GradeViewSet(viewsets.ModelViewSet):
+    queryset = Grade.objects.all()
+    serializer_class = GradeSerializer
+
+class SubjectSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Subject
+        fields = ('url', 'name')
+
+class SubjectViewSet(viewsets.ModelViewSet):
+    queryset = Subject.objects.all()
+    serializer_class = SubjectSerializer
+
+class LevelSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Level
+        fields = ('url', 'name')
+
+class LevelViewSet(viewsets.ModelViewSet):
+    queryset = Level.objects.all()
+    serializer_class = LevelSerializer
+
+class RoleSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Role
+        fields = ('url', 'name')
+
+class RoleViewSet(viewsets.ModelViewSet):
+    queryset = Role.objects.all()
+    serializer_class = RoleSerializer
+
+class ProfileSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ('url', 'name', 'role', 'gender', 'avatar',)
+
+class ProfileViewSet(viewsets.ModelViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+
+class TeacherSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Teacher
+        fields = ('url', 'user', 'name', 'degree', 'active', 'fulltime',
+                'schools',)
+
+class TeacherViewSet(viewsets.ModelViewSet):
+    queryset = Teacher.objects.all()
+    serializer_class = TeacherSerializer
+
+class WeeklyTimeSlotSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = WeeklyTimeSlot
+        fields = ('url', 'weekday', 'start', 'end',)
+
+class WeeklyTimeSlotViewSet(viewsets.ModelViewSet):
+    queryset = WeeklyTimeSlot.objects.all()
+    serializer_class = WeeklyTimeSlotSerializer
+
+
--- server/server/urls.py
+++ server/server/urls.py
@@ -16,105 +16,10 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from django.contrib.auth.models import User
-from rest_framework import routers, serializers, viewsets
+from rest_framework import routers
 from rest_framework.authtoken import views
 
-from app.models import *
-
-# Serializers define the API representation.
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ('url', 'username', 'email', 'is_staff', 'profile')
-
-# ViewSets define the view behavior.
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-class RegionSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Region
-        fields = ('url', 'name', 'superset', 'admin_level', 'leaf', 'weekly_time_slots')
-
-class RegionViewSet(viewsets.ModelViewSet):
-    queryset = Region.objects.all()
-    serializer_class = RegionSerializer
-
-class SchoolSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = School
-        fields = ('url', 'name', 'address', 'thumbnail', 'region', 'center',
-                'longitude', 'latitude',)
-
-class SchoolViewSet(viewsets.ModelViewSet):
-    queryset = School.objects.all()
-    serializer_class = SchoolSerializer
-
-class GradeSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Grade
-        fields = ('url', 'name', 'superset', 'leaf')
-
-class GradeViewSet(viewsets.ModelViewSet):
-    queryset = Grade.objects.all()
-    serializer_class = GradeSerializer
-
-class SubjectSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Subject
-        fields = ('url', 'name')
-
-class SubjectViewSet(viewsets.ModelViewSet):
-    queryset = Subject.objects.all()
-    serializer_class = SubjectSerializer
-
-class LevelSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Level
-        fields = ('url', 'name')
-
-class LevelViewSet(viewsets.ModelViewSet):
-    queryset = Level.objects.all()
-    serializer_class = LevelSerializer
-
-class RoleSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Role
-        fields = ('url', 'name')
-
-class RoleViewSet(viewsets.ModelViewSet):
-    queryset = Role.objects.all()
-    serializer_class = RoleSerializer
-
-class ProfileSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Profile
-        fields = ('url', 'name', 'role', 'gender', 'avatar',)
-
-class ProfileViewSet(viewsets.ModelViewSet):
-    queryset = Profile.objects.all()
-    serializer_class = ProfileSerializer
-
-class TeacherSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Teacher
-        fields = ('url', 'user', 'name', 'degree', 'active', 'fulltime',
-                'schools',)
-
-class TeacherViewSet(viewsets.ModelViewSet):
-    queryset = Teacher.objects.all()
-    serializer_class = TeacherSerializer
-
-class WeeklyTimeSlotSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = WeeklyTimeSlot
-        fields = ('url', 'weekday', 'start', 'end',)
-
-class WeeklyTimeSlotViewSet(viewsets.ModelViewSet):
-    queryset = WeeklyTimeSlot.objects.all()
-    serializer_class = WeeklyTimeSlotSerializer
+from app.views import *
 
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
@@ -136,5 +41,6 @@ urlpatterns = [
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^api/v1/token-auth/', views.obtain_auth_token),
     url(r'^api/v1/', include(router.urls)),
+    url(r'^$', 'app.views.index'),
     url(r'^', include('django.contrib.auth.urls')),
 ]
