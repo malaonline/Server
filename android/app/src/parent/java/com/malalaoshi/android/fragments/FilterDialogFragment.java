def065d032e7f563e1a414824b45a9ae24184580
Scott <zhonglijunyi@163.com>
2015-12-16 18:31:00 +0800

    remove filter dialog title divider

--- android/app/src/parent/java/com/malalaoshi/android/fragments/FilterDialogFragment.java
+++ android/app/src/parent/java/com/malalaoshi/android/fragments/FilterDialogFragment.java
@@ -119,6 +119,10 @@ public class FilterDialogFragment extends DialogFragment {
         window.setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.dialog_filter_title);
         ButterKnife.bind(this, getDialog());
         mTitleView.setText(FILTER_VIEW_TITLES[mFilterViews.getDisplayedChild()]);
+        int divierId = window.getContext().getResources().getIdentifier("android:id/titleDivider", null, null);
+        View divider = window.findViewById(divierId);
+//        divider.setBackgroundColor(Color.TRANSPARENT);
+        divider.setVisibility(View.INVISIBLE);
         getData();
     }
 
