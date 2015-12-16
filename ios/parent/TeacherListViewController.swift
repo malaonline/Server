a4c92e0b89295ffc6ae2f11fbd05020336220d8e
Shang Erdi <shangerdi@malalaoshi.com>
2015-12-16 21:37:24 +0800

    Init TeacherListView with simple image cell

--- ios/parent/TeacherListViewController.swift
+++ ios/parent/TeacherListViewController.swift
@@ -6,4 +6,33 @@
 //  Copyright © 2015 Mala Online. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class TeacherListViewController: UIViewController {
+
+  // static data for test
+  let image = UIImage(data: NSData(contentsOfURL: NSURL(string: "http://img1.touxiang.cn/uploads/20120419/19-020539_131.jpg")!)!)
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // todo: init the data source
+  }
+
+  func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int
+  {
+    return 100;
+  }
+
+  func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell
+  {
+    let cell: TeacherCollectionCell = collectionView.dequeueReusableCellWithReuseIdentifier("TeacherCollectionCell", forIndexPath: indexPath) as! TeacherCollectionCell
+    cell.imageView.image = image
+
+    return cell;
+  }
+
+  func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath)
+  {
+    print("select cell: \(indexPath.row)")
+  }
+}
