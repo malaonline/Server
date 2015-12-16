80b1ac2a341753a14bc40a20420d9546382c223a
Leon <i@leons.im>
2015-12-16 21:24:14 +0800

    Fix import issue.

--- android/app/src/parent/java/com/malalaoshi/android/MainActivity.java
+++ android/app/src/parent/java/com/malalaoshi/android/MainActivity.java
@@ -15,6 +15,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.malalaoshi.android.fragments.FilterDialogFragment;
 import com.malalaoshi.android.fragments.LoginFragment;
