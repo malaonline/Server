289dcf0a5ede45c7c39c6bf6a98cd754ba6bc710
Scott <zhonglijunyi@163.com>
2015-12-22 15:52:12 +0800

    refactor DialogFragment

---
+++ android/app/src/main/java/com/malalaoshi/android/base/BaseDialogFragment.java
@@ -0,0 +1,30 @@
+package com.malalaoshi.android.base;
+
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.view.Window;
+
+import com.malalaoshi.android.R;
+
+/**
+ * Created by liumengjun on 12/22/15.
+ */
+public class BaseDialogFragment extends DialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Dialog dialog = super.onCreateDialog(savedInstanceState);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.getWindow().setBackgroundDrawableResource(R.drawable.bg_rounded_corners);
+        return dialog;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        Window window = getDialog().getWindow();
+        int width = getResources().getDimensionPixelSize(R.dimen.dialog_width);
+        window.setLayout(width, window.getAttributes().height);
+    }
+}
