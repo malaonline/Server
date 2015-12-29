3e84ffd2b592251bc3ef5076c50b5a73ef535bcb
Leon <i@leons.im>
2015-12-29 15:39:12 +0800

    Fix deploy script.

--- server/deploy.sh
+++ server/deploy.sh
@@ -4,13 +4,14 @@ DES=/opt/jenkins/mala/server
 ENV=/opt/jenkins/env
 SET=/opt/keys-pros
 
+rm -rf $DES
 mkdir -p $DES
-rm -rf $DES/*
-cp -Rf server/* $DES/
+cp -Rf * $DES/
 cp -Rf $SET/local_settings.py $DES/
 
-cd $DES
 . $ENV/bin/activate
+
+cd $DES
 pip install -r pip_install.txt
 python manage.py migrate
 if [ -n "`ps aux | grep gunicorn | grep server.wsgi| awk '{ print $2 }'`" ]
