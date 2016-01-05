968fd4e8894693ded6caaafeff35e4b0ae670fbb
Leon <i@leons.im>
2016-01-05 23:47:16 +0800

    Update build script.

--- ios/buildbot.sh
+++ ios/buildbot.sh
@@ -4,13 +4,13 @@
 
 buildTime=$(date +%Y%m%d%H%M)
 
-profile="For test"
-
 buildConfiguration="QA"
 buildPath="AutoBuild/ArchiveProduction/QA_${buildTime}.xcarchive"
 ipaName="AutoBuild/IPA/Auto_QA_${buildTime}.ipa"
 mkdir -p AutoBuild/IPA
 
+security -v unlock-keychain -p ${KEYCHAIN_PASSWORD} ${KEYCHAIN_PATH}
+
 xctool -workspace mala-ios.xcworkspace -scheme parent -configuration Release clean
 xctool -workspace mala-ios.xcworkspace -scheme parent -configuration Release archive -archivePath ${buildPath}
-xcodebuild -exportArchive -exportFormat IPA -archivePath ${buildPath} -exportPath ${ipaName} -exportProvisioningProfile "${profile}"
+xcodebuild -exportArchive -exportFormat IPA -archivePath ${buildPath} -exportPath ${ipaName} -exportProvisioningProfile ${PROVISIONING_PROFILE}
