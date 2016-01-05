ae2400ad8687063a6af0bea0c882c5df7e46dfc7
Leon <i@leons.im>
2016-01-06 00:04:58 +0800

    Update build script.

--- ios/buildbot.sh
+++ ios/buildbot.sh
@@ -3,14 +3,14 @@
 # I am AutoBuildBot!
 
 buildTime=$(date +%Y%m%d%H%M)
+schema="parent"
 
-buildConfiguration="QA"
-buildPath="AutoBuild/ArchiveProduction/QA_${buildTime}.xcarchive"
-ipaName="AutoBuild/IPA/Auto_QA_${buildTime}.ipa"
+buildPath="AutoBuild/ArchiveProduction/${schema}_${buildTime}.xcarchive"
+ipaName="AutoBuild/IPA/${schema}_${buildTime}.ipa"
 mkdir -p AutoBuild/IPA
 
 security -v unlock-keychain -p ${KEYCHAIN_PASSWORD} ${KEYCHAIN_PATH}
 
-xctool -workspace mala-ios.xcworkspace -scheme parent -configuration Release clean
-xctool -workspace mala-ios.xcworkspace -scheme parent -configuration Release archive -archivePath ${buildPath}
+xctool -workspace mala-ios.xcworkspace -scheme ${schema} -configuration Release clean
+xctool -workspace mala-ios.xcworkspace -scheme ${schema} -configuration Release archive -archivePath ${buildPath}
 xcodebuild -exportArchive -exportFormat IPA -archivePath ${buildPath} -exportPath ${ipaName} -exportProvisioningProfile "For jenkins"
