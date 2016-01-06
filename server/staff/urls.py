002420b04998e70f5c18668eba95e317a7573c65
mengjun <zhonglijunyi@163.com>
2016-01-06 12:33:00 +0800

    BE-1: add login auth view

--- server/staff/urls.py
+++ server/staff/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name="index"),
     url(r'^login/$', views.login, name="login"),
+    url(r'^login/auth/$', views.login_auth, name="login_auth"),
 ]
--- server/staff/views.py
+++ server/staff/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
-from .decorators import mala_staff_required
+import logging
+
+# django modules
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse
+from django.views.decorators.http import require_POST
+from django.contrib import auth
+
+# local modules
+from app import models
+from .decorators import *
+
+logger = logging.getLogger('app')
 
 # Create your views here.
 
@@ -8,5 +18,25 @@ from django.http import HttpResponse, JsonResponse
 def index(request):
     return render(request, 'staff/index.html')
 
-def login(request):
-    return render(request, 'staff/login.html')
+def login(request, context={}):
+    return render(request, 'staff/login.html', context)
+
+@require_POST
+def login_auth(request):
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    goto_page = request.POST.get('next')
+    logger.debug('try to login, username: '+username+', password: '+password+', goto_page: '+str(goto_page))
+    if not username or not password:
+        return login(request, {'errors': '请输入用户名和密码'})
+    #登录前需要先验证
+    newUser=auth.authenticate(username=username,password=password)
+    if newUser is not None:
+        if not is_manager(newUser):
+            return login(request, {'errors': '你不是管理员呀'})
+        auth.login(request, newUser)
+        if goto_page:
+            return redirect(goto_page)
+        else:
+            return redirect('staff:index')
+    return login(request, {'errors': '用户名或密码错误'})
