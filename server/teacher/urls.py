099d88cf05d75bb54d1e68f65d0635e678688ccd
曹亚文 <caoyawen@malalaoshi.com>
2016-01-06 11:42:55 +0800

    rename chinese file. remove doc generation html. use text to html instead. add file map between one documentation's Chinese name and English name.

--- server/teacher/urls.py
+++ server/teacher/urls.py
@@ -5,6 +5,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.register, name="register"),
-    url(r'^doc/agree/$', TemplateView.as_view(template_name="teacher/doc/麻辣老师服务协议.htm"), name="doc-agree"),
+    url(r'^doc/agree/$', TemplateView.as_view(template_name="teacher/doc/policy.html"), name="doc-agree"),
 
 ]
