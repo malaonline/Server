a65805bd44c890e050d6e7594f39b9a7c9652bc8
mengjun <zhonglijunyi@163.com>
2016-01-06 17:45:57 +0800

    BE-23: add test users into group

--- server/app/management/commands/add_groups_to_sample_users.py
+++ server/app/management/commands/add_groups_to_sample_users.py
@@ -14,7 +14,7 @@ class Command(BaseCommand):
         write("run {command_name}".format(command_name=build_group_command))
         call_command(build_group_command,)
 
-        write("add parent group to parent users")
+        write("add 家长测试用户 into Group of 家长...")
         parent_name_formula = settings.SAMPLE_PARENT_USER_FORMULA
         group_name = "家长"
         parent_group = Group.objects.get(name=group_name)
@@ -28,4 +28,20 @@ class Command(BaseCommand):
             parent_user.groups.add(parent_group)
             parent_user.save()
             print("{user} added {group}".format(user=parent_user, group=parent_group))
+        write("add 家长测试用户 into Group of 家长 end.")
 
+        write("add 老师测试用户 into Group of 老师...")
+        teacher_name_formula = settings.SAMPLE_TEACHER_USER_FORMULA
+        group_name = "老师"
+        teacher_group = Group.objects.get(name=group_name)
+        for i in range(settings.SAMPLE_DATA_LENGTH):
+            username = teacher_name_formula.format(id=i)
+            try:
+                teacher_user = User.objects.get(username=username)
+            except User.DoesNotExist:
+                write("{user} not exist".format(teacher_name_formula))
+                continue
+            teacher_user.groups.add(teacher_group)
+            teacher_user.save()
+            print("{user} added {group}".format(user=teacher_user, group=teacher_group))
+        write("add 老师测试用户 into Group of 老师 end.")
