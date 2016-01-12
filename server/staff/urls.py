a3e04ba6af57cbf1b472860dee3bbaaeadc6a8c2
jackyZhang <zhangzhengliang@malalaoshi.com>
2016-01-12 18:50:55 +0800

    BE-6:school center view

--- server/staff/urls.py
+++ server/staff/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     url(r'^logout/$', views.logout, name="logout"),
     url(r'^students/$', views.StudentView.as_view(), name="students"),
     url(r'^teachers/$', views.TeacherView.as_view(), name="teachers"),
+    url(r'^schools/$', views.SchoolView.as_view(), name="schools"),
 ]
