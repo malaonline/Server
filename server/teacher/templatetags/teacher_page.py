5e8e7d6d55fe1d143724f6187699c804b4ea47ad
曹亚文 <caoyawen@malalaoshi.com>
2016-01-14 18:00:17 +0800

    TWEB-11, TWEB-12, TWEB-13, move static file psition.

--- server/teacher/templatetags/teacher_page.py
+++ server/teacher/templatetags/teacher_page.py
@@ -5,6 +5,9 @@ register = template.Library()
 
 
 @register.inclusion_tag("teacher/tag/mala_header.html", takes_context=True)
-def mala_page_header(context, title):
-    return {"title": title}
+def mala_page_header(context, title, user_name=None):
+    return {
+        "title": title,
+        "user_name": user_name
+    }
 
