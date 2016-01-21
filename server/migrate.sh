cdd3fd394f655f805946c50b12ef0cd5fb863f48
Leon <i@leons.im>
2016-01-21 18:22:08 +0800

    SERVER-64 fix scripts.

--- server/migrate.sh
+++ server/migrate.sh
@@ -12,9 +12,4 @@ cp -Rf $SET/local_settings.py $DES/server/
 
 cd $DES
 pip install -r pip_install.txt
-python manage.py migrate
-python manage.py compilestatic
-python manage.py collectstatic --noinput
-python manage.py build_groups_and_permissions
-python manage.py add_groups_to_sample_users
 python manage.py test --noinput
