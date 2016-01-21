e4383cd1fe01cc9b4abf574dd264178f7221d66c
Leon <i@leons.im>
2016-01-21 18:00:28 +0800

    Merge migrations.

---
+++ server/app/migrations/0038_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('app', '0037_coupon'),
+        ('app', '0037_auto_20160121_1227'),
+    ]
+
+    operations = [
+    ]
