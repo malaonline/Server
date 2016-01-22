24cea1a5b9ecb54fc97ccf7629a108e0910f1bcf
曹亚文 <caoyawen@malalaoshi.com>
2016-01-22 18:41:56 +0800

    TWEB-13 set other region's leaf to True. rename 0041_add_other_region to 0042_add_other_region.

--- server/app/migrations/0042_add_other_region.py
+++ server/app/migrations/0042_add_other_region.py
@@ -5,14 +5,14 @@ def add_other_region(apps, schema_editor):
     Region = apps.get_model('app', 'Region')
     # 这是一个省级的对象
     # 相当于 "其它": {}
-    region = Region(name="其它", superset=None, admin_level=1, leaf=False)
+    region = Region(name="其它", superset=None, admin_level=1, leaf=True)
     region.save()
     print("添加地区-其它")
 
 
 class Migration(migrations.Migration):
     dependencies = [
-        ('app', '0040_superuser'),
+        ('app', '0041_policy'),
     ]
     operations = [
         migrations.RunPython(add_other_region)
