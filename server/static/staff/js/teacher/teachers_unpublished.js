8b3fa54f7a481a23ae208c5000d88073582ca5cf
mengjun <zhonglijunyi@163.com>
2016-01-23 12:40:37 +0800

    BE-2: fulfill teacher unpublished data schools

--- server/static/staff/js/teacher/teachers_unpublished.js
+++ server/static/staff/js/teacher/teachers_unpublished.js
@@ -33,11 +33,30 @@ $(function(){
 
     paginationInit();
 
-    $('a[data-action=show-avatar]').click(function(e){
+    // 预览头像
+    $('[data-action=show-avatar]').click(function(e){
         var url = $(this).attr('url');
         var $modal = $("#avatarModal");
         $modal.find('img').attr('src', url);
         $modal.modal();
     });
-
+    // 查看学习中心
+    $('[data-action=show-schools').click(function(e){
+        var $schoolsModal = $("#schoolsModal");
+        $.getJSON('/api/v1/schools/',function(data){
+            if (data && data.results)  {
+                var $schoolsTable = $schoolsModal.find("table");
+                $schoolsTable.find('tr:gt(0)').remove();
+                for (var i=0; i<data.results.length; i++) {
+                    var school = data.results[i];
+                    $schoolsTable.append('<tr schoolId="'+school.id+'">' +
+                        '<td>'+school.name+'</td>' +
+                        '<td><img src="'+school.thumbnail+'" height="66" style="max-height: 100%; max-width: 100%; vertical-align: middle"></td>' +
+                        '<td>'+school.address+'</td>' +
+                        '<td>TODO</td></tr>');
+                }
+                $schoolsModal.modal();
+            }
+        });
+    });
 });
