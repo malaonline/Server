993358785eaef5e3fb4684f0f551553a6c916689
mengjun <zhonglijunyi@163.com>
2016-01-26 12:20:50 +0800

    BE-2: teacher edit page, introduction audio-video

--- server/static/staff/js/teacher/teacher_edit.js
+++ server/static/staff/js/teacher/teacher_edit.js
@@ -14,35 +14,15 @@ $(function(){
       return url;
     };
     // input file changed event
-    $('input[type=file]').change(function(e){
+    $('.img-edit input[type=file]').change(function(e){
         var ele = e.target, $ele = $(ele);
         var $editBox = $ele.closest(".img-edit"), $uploadBox = $editBox.find(".img-upload-box");
         $editBox.find('.help-block').hide();
-        var imgType = ["gif", "jpeg", "jpg", "bmp", "png"];
-        var flag = validImgFile();
-        if (!flag) {
-          return false;
-        }
         var imtUrl = getObjectURL(ele.files[0]);
         var $previewBox = $uploadBox.find(".img-preview-box");
         $previewBox.find('img').attr("src", imtUrl);
         $previewBox.show();
         return true;
-
-        // valid image properties
-        function validImgFile() {
-          if (!ele.value || !ele.files) {
-            return false;
-          }
-          //验证上传文件格式是否正确
-          if (!RegExp("\.(" + imgType.join("|") + ")$", "i").test(ele.value.toLowerCase())) {
-            $editBox.addClass('has-error');
-            $editBox.find('.help-block').text('选择图片类型错误').show();
-            this.value = "";
-            return false;
-          }
-          return true;
-        }
     });
 
     $('[data-action=add-more-photo]').click(function(){
@@ -64,4 +44,25 @@ $(function(){
         $certEdit.after($newCertEdit);
     });
 
+    // audio file changed event
+    $('.media-edit input[type=file]').change(function(e){
+        var ele = e.target, $ele = $(ele), $editBox = $ele.closest(".media-edit");
+        $editBox.find('.help-block').hide();
+        var type='', file = ele.files[0];
+        if ($editBox.hasClass('audio-edit')) {
+            type = 'audio';
+        } else if ($editBox.hasClass('video-edit')) {
+            type = 'video';
+        }
+        var $uploadBox = $editBox.find("."+type+"-upload-box");
+        var mediaUrl = getObjectURL(file);
+        var $previewBox = $uploadBox.find(type+".preview");
+        $previewBox.attr("src", mediaUrl);
+        $previewBox.show();
+        var audio = $previewBox[0];
+        //audio.play();
+        function g(){isNaN(audio.duration) ? requestAnimationFrame(g):console.info("该歌曲的总时间为："+audio.duration+"秒")}
+        requestAnimationFrame(g);
+        return true;
+    });
 });
