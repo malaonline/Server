d392010d8e322e368ecbb79e2ab34069e992fa41
Leon <i@leons.im>
2016-01-30 18:43:45 +0800

    SERVER-77 Seperate migration script with deployment script.

---
+++ server/migrate.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+DES=/opt/jenkins/mala/server
+ENV=/opt/jenkins/env
+SET=/opt/keys-pros
+
+mkdir -p $DES
+rsync -r --delete * $DES/
+cp -Rf $SET/local_settings.py $DES/server/
+
+. $ENV/bin/activate
+pip install -r pip_install.txt
+
+cd $DES
+
+python manage.py migrate
+python manage.py compilestatic
+python manage.py collectstatic --noinput
+python manage.py mala_all
