4839ce565fcbacf02f63c4db4d722cedd9ca0488
Mengjun Liu <liumengjun@malalaoshi.com>
2016-01-30 18:13:50 +0800

    BE-2: rename teacher public to published

---
+++ server/app/migrations/0057_auto_20160130_1812.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('app', '0056_auto_20160128_1944'),
+    ]
+
+    operations = [
+        migrations.RenameField(
+            model_name='teacher',
+            old_name='public',
+            new_name='published',
+        ),
+    ]
