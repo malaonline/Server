79b12ae209b3de4dfd0cb13d2e46b4e1c38bcb5c
曹亚文 <caoyawen@malalaoshi.com>
2016-02-01 02:13:27 +0800

    TWEB-25, add missing file.

---
+++ server/static/teacher/js/custom/my_school_timetable.js
@@ -0,0 +1,33 @@
+/**
+ * Created by caoyawen on 16/2/1.
+ */
+$(
+    function(){
+        console.log("TW-5-1");
+
+    }
+);
+
+function day_cell_hover(day_id, today_date, today_week_day){
+    console.log(day_id);
+    $("#today-date").html(today_date);
+    $("#today-week-day").html(today_week_day);
+    var header_template = $("#time_slot_header_template").html();
+    var header_content = _.template(header_template, {variable: 'datas'})({today_date:today_date,
+        today_week_day:today_week_day});
+    if (window.time_slot_data[day_id] == undefined){
+    //  今天暂无课程
+        console.log("no-class");
+        var no_class_template = $("#today_has_no_details").html();
+        var no_class_content = _.template(no_class_template)();
+        var list_content = header_content+no_class_content;
+        $("#today-time-slot-list").html(list_content);
+    }else{
+    //  有课程,需要跟着模版走
+        var time_slot_list_template = $("#time_slot_list_template").html();
+        var time_slot_list_content = _.template(time_slot_list_template, {variable: 'datas'})(window.time_slot_data[day_id]);
+        var list_content = header_content+time_slot_list_content;
+        $("#today-time-slot-list").html(list_content);
+
+    }
+}
---
+++ server/teacher/management/commands/create_fake_order.py
@@ -0,0 +1,86 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User, Group
+# import django.contrib.auth.models.DoesNotExist
+from django.contrib.auth.hashers import make_password
+from django.utils.timezone import make_aware
+
+import datetime
+
+from app.models import Teacher, Order, School, Region, Profile, Parent, Grade, Subject, TimeSlot
+
+
+class Command(BaseCommand):
+    help = "Add fake order to exists teacher"
+
+    def fake_school(self):
+        """
+        获得一个伪造的学校
+        :return: 伪造的学校
+        """
+        school_name = "逗比中学"
+        school_address = "逗比路"
+        try:
+            school = School.objects.get(name=school_name)
+        except School.DoesNotExist:
+            school = School(name=school_name,
+                            address=school_address, region=Region.objects.get(name="其他"),
+                            center=True, longitude=0, latitude=0, opened=False)
+            school.save()
+        return school
+
+    def fake_parent(self):
+        """
+        获得伪造的家长
+        :return:
+        """
+        parent_user_name = "fake_parent_username"
+
+        parent_name = "张三的家长"
+        parent_password = "fake parent's password"
+        parent_salt = "fake parent's salt"
+        parent_email = "fake-parent@email.com"
+        student_name = "张三"
+        student_school_name = "张三的学校"
+
+        try:
+            parent_user = User.objects.get(username=parent_user_name)
+        except User.DoesNotExist:
+            parent_user = User(username=parent_user_name)
+            parent_user.password = make_password(parent_password, parent_salt)
+            parent_user.email = parent_email
+            parent_user.save()
+            parent_group = Group.objects.get(name="家长")
+            parent_user.groups.add(parent_group)
+            parent_group.save()
+        try:
+            parent_profile = Profile.objects.get(user=parent_user)
+        except Profile.DoesNotExist:
+            parent_profile = Profile(user=parent_user, phone="18922405996")
+            parent_profile.save()
+        try:
+            parent = Parent.objects.get(user=parent_user)
+        except Parent.DoesNotExist:
+            parent = Parent(user=parent_user, student_name=student_name, student_school_name=student_school_name)
+            parent.save()
+        return parent
+
+    def handle(self, *args, **options):
+        for one_teacher in Teacher.objects.all():
+            if len(Order.objects.filter(teacher=one_teacher).all()) == 0:
+                school = self.fake_school()
+                parent = self.fake_parent()
+                order = Order(parent=parent, teacher=one_teacher, school=school, grade=Grade.objects.get(name="一年级"),
+                              subject=Subject.objects.get(name="数学"), coupon=None, price=200, hours=50, total=100,
+                              paid_at=make_aware(datetime.datetime.now()), status=Order.PAID)
+                order.save()
+                one_time_slot = TimeSlot(order=order, start=make_aware(datetime.datetime(2016, 1, 1, 8, 0, 0)),
+                                         end=make_aware(datetime.datetime(2016, 1, 1, 10, 0, 0)))
+                one_time_slot.save()
+                one_time_slot = TimeSlot(order=order, start=make_aware(datetime.datetime(2015, 12, 30, 15, 0, 0)),
+                                         end=make_aware(datetime.datetime(2015, 12, 30, 17, 0, 0)))
+                one_time_slot.save()
+                one_time_slot = TimeSlot(order=order, start=make_aware(datetime.datetime(2015, 12, 20, 11, 0, 0)),
+                                         end=make_aware(datetime.datetime(2015, 12, 20, 12, 0, 0)))
+                one_time_slot.save()
+
+
