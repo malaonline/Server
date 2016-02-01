b4af93a04cc4c6e7d25ceeab1f032bc8dffe61d9
Mengjun Liu <liumengjun@malalaoshi.com>
2016-02-01 17:11:00 +0800

    BE-3: fix parseInt(None) exception

--- server/app/utils/types.py
+++ server/app/utils/types.py
@@ -2,6 +2,7 @@ import re
 
 def parseInt(nums, default='NaN'):
     """
+    (parseInt(None) == 'NaN')
     (parseInt('') == 'NaN')
     (parseInt(123) == 123)
     (parseInt(-123) == -123)
@@ -16,11 +17,11 @@ def parseInt(nums, default='NaN'):
     (parseInt('asd') == 'NaN')
     (parseInt('-asd') == 'NaN')
     """
+    if nums is None or nums is '':
+        return default
     if isinstance(nums, int):
         return nums
     if isinstance(nums, float):
         return int(nums)
-    if not nums:
-        return default
     d = re.search(r'^-?\d+', nums)
     return int(d.group(0)) if d else default
