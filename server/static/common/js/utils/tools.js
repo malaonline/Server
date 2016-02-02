7dc14bb43749a70fde5c72a60d1cf50c62d9c885
曹亚文 <caoyawen@malalaoshi.com>
2016-02-02 11:15:59 +0800

    TWEB-50, simplify the code as mengjun's suggestion.

--- server/static/common/js/utils/tools.js
+++ server/static/common/js/utils/tools.js
@@ -6,5 +6,5 @@
 //错误返回false
 function checkMobile(phone_val){
     var pattern=/(^(([0\+]\d{2,3}-)?(0\d{2,3})-)(\d{7,8})(-(\d{3,}))?$)|(^0{0,1}1[3|4|5|6|7|8|9][0-9]{9}$)/;
-    return !!pattern.test(phone_val);
+    return pattern.test(phone_val);
 }
