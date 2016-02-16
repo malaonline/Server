39eb63e4bf569c226a09041a7c9b0d87b23c287e
jackyZhang <zhangzhengliang@malalaoshi.com>
2016-02-16 18:19:02 +0800

    TWEB-20:teacher add gradute school

--- server/app/models.py
+++ server/app/models.py
@@ -264,6 +264,8 @@ class Teacher(BaseModel):
                                limit_choices_to={'opened': True})
     status = models.IntegerField(default=1, choices=STATUS_CHOICES)
 
+    graduate_school = models.CharField(max_length=50, default=None, null=True)
+
     def __str__(self):
         return '%s %s %s' % (self.name, 'F' if self.fulltime else '',
                              'Unpublished' if not self.published else '')
