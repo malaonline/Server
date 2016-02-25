6f3448691f29b8ba6a16a0505c4299feca5a3b09
walter-wu <walter@malalaoshi.com>
2016-02-25 17:47:51 +0800

    WEC-13 测试weui

--- server/wechat/views.py
+++ server/wechat/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+import  logging
 
+# django modules
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import View, TemplateView, ListView
+from django.db.models import Q
+from django.utils import timezone
+
+
+# local modules
+from app import models
 # Create your views here.
+
+
+class TeachersView(ListView):
+    model = models.Coupon
+    template_name = 'wechat/teacher/teachers.html'
