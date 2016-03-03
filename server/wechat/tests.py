62c951552192017540d9a43e8bf26275d789808f
walter-wu <walter@malalaoshi.com>
2016-03-03 16:10:00 +0800

    WEC-4 add unit test to wechat pages.

--- server/wechat/tests.py
+++ server/wechat/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.core.urlresolvers import reverse
+from django.conf import settings
 
-# Create your tests here.
+class TestWechatPage(TestCase):
+    def test_teachers(self):
+        client = Client()
+        response = client.get(reverse('wechat:teachers'))
+        self.assertEqual(response.status_code, 200)
+    def TestWechatSchools(self):
+        client = Client()
+        response = client.get(reverse('wechat:schools'))
+        self.assertEqual(response.status_code, 200)
