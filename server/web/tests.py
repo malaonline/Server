6ffdbd3389e61f9062fc1b156013f24351268f10
walter-wu <walter@malalaoshi.com>
2016-03-05 14:21:10 +0800

    OW-3 add test

--- server/web/tests.py
+++ server/web/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+from django.test import TestCase, Client
+from django.core.urlresolvers import reverse
+from django.conf import settings
+
+class TestIndex(TestCase):
+    def test_index(self):
+        client = Client()
+        response = client.get(reverse('web:index'))
+        self.assertEqual(response.status_code, 200)
+    def test_index_teacher(self):
+        client = Client()
+        response = client.get(reverse('web:teacher-index'))
+        self.assertEqual(response.status_code, 200)
