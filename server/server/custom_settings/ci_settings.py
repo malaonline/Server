af681acca33cfa044d9c17939a65dcc478c408e5
曹亚文 <caoyawen@malalaoshi.com>
2016-03-07 11:57:13 +0800

    SERVER-103, use postgresql instead of the sqlite on ci test.

--- server/server/custom_settings/ci_settings.py
+++ server/server/custom_settings/ci_settings.py
@@ -6,12 +6,12 @@ from ..settings import *
 FAKE_SMS_SERVER = True
 
 # 用sqlite替代postgresql
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 # 把上传路径改为本地
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
