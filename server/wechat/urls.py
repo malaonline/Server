eca7e63acaf98fbc8c94f5f36a59fa341642ac43
jackyZhang <zhangzhengliang@malalaoshi.com>
2016-03-07 18:17:13 +0800

    WEC-10: send weixin template message

--- server/wechat/urls.py
+++ server/wechat/urls.py
@@ -7,6 +7,5 @@ urlpatterns = [
     url(r'^teachers/$', views.TeachersView.as_view(), name="teachers"),
     url(r'^teachers/detail/(?P<id>[0-9]+)/$', views.TeacherDetailView.as_view(), name="teacher-detail"),
     url(r'^send_template_ms/$', views.send_template_msg, name="send_template_msg"),
-]
     url(r'^order/course_choosing/(?P<teacher_id>[0-9]+)/$', views.CourseChoosingView.as_view(), name="order-course-choosing"),
 ]
