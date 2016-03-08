346076ad3caa059d8ee0145dc773573071e00672
walter-wu <walter@malalaoshi.com>
2016-03-08 11:45:06 +0800

    OW-3 bring better view on mobile screen.

--- server/static/web/js/index_teacher_fullpage.js
+++ server/static/web/js/index_teacher_fullpage.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 			$('#fullpage').fullpage({
 				anchors: ['firstPage', 'secondPage', '3rdPage','4thPage','footer'],
-				sectionsColor: ['#1acbe9', '#17cc83', '#f5c56f','#1acbe9','#2f3440'],
+				sectionsColor: ['#48cbec', '#42cc7f', '#f1c566','#1acbe9','#2f3440'],
 				responsiveHeight: 600,
                 navigation: true,
 				navigationPosition: 'right',
