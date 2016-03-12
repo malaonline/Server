02640a05b486c7a65e445017e4b1893e982dcf3c
Mengjun Liu <liumengjun@malalaoshi.com>
2016-03-12 15:18:18 +0800

    BE-44: set cert verified default False

--- server/app/models.py
+++ server/app/models.py
@@ -769,7 +769,7 @@ class Certificate(BaseModel):
     name = models.CharField(max_length=100)
     type = models.IntegerField(null=True, blank=True, choices=TYPE_CHOICES)
     img = models.ImageField(null=True, blank=True, upload_to='certs')
-    verified = models.BooleanField()
+    verified = models.BooleanField(default=False)
 
     def __str__(self):
         return '%s, %s : %s' % (self.teacher, self.name,
