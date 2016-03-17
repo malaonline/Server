0cc61d09b4ee64689a21de5b889d12370b5b8059
walter-wu <walter@malalaoshi.com>
2016-03-17 14:33:14 +0800

    WEC-21 implement photo album preview popup.

--- server/static/wechat/js/schools.js
+++ server/static/wechat/js/schools.js
@@ -2,24 +2,11 @@
 
 //测试用:大望路附近坐标:39.913608,116.482342
 
-/*
-$(document).on('click','[data-schoolid]',function (evt) {
-    //photosdic = JSON.parse(photosdic);
-    console.log(photosdic)
-    schoolId = parseInt(evt.currentTarget.dataset.schoolid);
-    photoList = photosdic[schoolId] || [];
-    console.log('school的编号:'+schoolId+', URL列表: '+photoList)
-});
-*/
-
 $(function(){
     $(document).on('click','[data-schoolid]',function (evt) {
-        //photosdic = JSON.parse(photosdic);
-        console.log(photosdic)
         schoolId = parseInt(evt.currentTarget.dataset.schoolid);
         photoList = photosdic[schoolId] || [];
-        console.log('school的编号:'+schoolId+', URL列表: '+photoList)
-
+        //console.log('school的编号:'+schoolId+', URL列表: '+photoList)
 
         var schoolPhotosPopup = $.photoBrowser({
             photos : photoList,
