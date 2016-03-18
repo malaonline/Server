cfcf7950ca0bd38ffd3042ccdb8a73601a328e1c
Leon <i@leons.im>
2016-03-18 17:21:57 +0800

    AN-171 Build two release apk at the same time.

--- android/buildbot.sh
+++ android/buildbot.sh
@@ -3,5 +3,4 @@
 # gradle build
 cp /opt/keys-pros/*.properties ./
 # no teacher client temporarily
-gradle assembleParentDevrelease
-gradle assembleParentRelease
+gradle assembleParentDevrelease assembleParentRelease
