1f90f7042235d6208037f9fbabfe026799e7d9c2
Shang Erdi <shangerdi@malalaoshi.com>
2016-03-18 14:20:57 +0800

    BE-105: Hot fix for refund

--- server/app/models.py
+++ server/app/models.py
@@ -1,28 +1,26 @@
-import logging
 import datetime
-
+import logging
 import posix_ipc
-from segmenttree import SegmentTree
-
 import random
 
-from django.contrib.auth.models import User
-from django.db import models
-from django.db import IntegrityError, transaction
+from django.apps import apps
+from django.conf import settings
+from django.contrib.auth import authenticate
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import Group
-from django.contrib.auth import authenticate
-from django.apps import apps
+from django.contrib.auth.models import User
+from django.db import IntegrityError, transaction
+from django.db import models
+from django.db.models import Q
 from django.utils import timezone
 from django.utils.timezone import make_aware, localtime
-from django.conf import settings
-from django.db.models import Q
+from segmenttree import SegmentTree
 
 from app.exception import TimeSlotConflict, OrderStatusIncorrect, RefundError
-from app.utils.algorithm import orderid, Tree, Node
 from app.utils import random_string, classproperty
+from app.utils.algorithm import orderid, Tree, Node
 from app.utils.smsUtil import isTestPhone, sendCheckcode, SendSMSError,\
-        tpl_send_sms, TPL_COURSE_INCOME, TPL_STU_PAY_SUCCESS, TPL_TEACHER_COURSE_PAID
+        tpl_send_sms, TPL_STU_PAY_SUCCESS, TPL_TEACHER_COURSE_PAID
 
 logger = logging.getLogger('app')
 
@@ -1384,13 +1382,15 @@ class OrderManager(models.Manager):
                     assert TimeSlot.objects.all().filter(
                         order=order,
                         deleted=False,
-                        end__gt=record.created_at - TimeSlot.CONFIRM_TIME
+                        accounthistory__isnull=True,
+                        order__status=Order.PAID
                     ).count() * 2 == record.remaining_hours
                     # 释放该订单内的所有未完成的课程时间
                     TimeSlot.objects.all().filter(
                         order=order,
                         deleted=False,
-                        end__gt=record.created_at - TimeSlot.CONFIRM_TIME
+                        accounthistory__isnull=True,
+                        order__status=Order.PAID
                     ).update(deleted=True)
                 else:
                     coupon = order.coupon
