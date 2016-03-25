1c8660e5e11370607e5b388925c245242f3f0694
曹亚文 <caoyawen@malalaoshi.com>
2016-03-25 17:32:23 +0800

    TWEB-128, remove console log.

--- server/static/teacher/js/custom/my_level.js
+++ server/static/teacher/js/custom/my_level.js
@@ -7,16 +7,16 @@ $(
             console.log("click");
             var expand_button = $("#expand_record");
             var rest_part_record = $('#rest_part_record');
-            console.log("expand_record is "+window.expand_record_tag);
+            // console.log("expand_record is "+window.expand_record_tag);
             if (window.expand_record_tag == undefined || window.expand_record_tag == false){
             //    展开
-                console.log("展开");
+            //     console.log("展开");
                 window.expand_record_tag = true;
                 expand_button.html("-");
                 rest_part_record.attr('attrHidden', false);
             }else{
             //    收起
-                console.log("收起");
+            //     console.log("收起");
                 window.expand_record_tag = false;
                 expand_button.html("+");
                 rest_part_record.attr('attrHidden', true);
