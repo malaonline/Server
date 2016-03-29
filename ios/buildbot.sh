7186c42a3d0bceabd70319aa235419d8b024e43a
Leon <i@leons.im>
2016-03-29 21:59:28 +0800

    IOS-242 CI should fail.

--- ios/buildbot.sh
+++ ios/buildbot.sh
@@ -9,19 +9,19 @@ security -v unlock-keychain -p ${KEYCHAIN_PASSWORD} ${KEYCHAIN_PATH}
 security set-keychain-settings -l -u -t 3600 ${KEYCHAIN_PATH}
 security list-keychains -s ${KEYCHAIN_PATH}
 
-configuration="PrdRelease"
-buildPath="build/archive/${scheme}_prd_release.xcarchive"
-ipaName="${ipaDir}${scheme}_prd_release.ipa"
-
-xctool -workspace mala-ios.xcworkspace -scheme ${scheme} -configuration ${configuration} archive -archivePath ${buildPath}
-rm -f ${ipaName}
-xcodebuild -exportArchive -exportFormat IPA -archivePath ${buildPath} -exportPath ${ipaName} -exportProvisioningProfile "${provisioning}"
-
-
 configuration="DevRelease"
 buildPath="build/archive/${scheme}_dev_release.xcarchive"
 ipaName="${ipaDir}${scheme}_dev_release.ipa"
 
-xctool -workspace mala-ios.xcworkspace -scheme ${scheme} -configuration ${configuration} archive -archivePath ${buildPath}
+xctool -workspace mala-ios.xcworkspace -scheme ${scheme} -configuration ${configuration} test archive -archivePath ${buildPath}
+rm -f ${ipaName}
+xcodebuild -exportArchive -exportFormat IPA -archivePath ${buildPath} -exportPath ${ipaName} -exportProvisioningProfile "${provisioning}"
+
+
+configuration="PrdRelease"
+buildPath="build/archive/${scheme}_prd_release.xcarchive"
+ipaName="${ipaDir}${scheme}_prd_release.ipa"
+
+xctool -workspace mala-ios.xcworkspace -scheme ${scheme} -configuration ${configuration} test archive -archivePath ${buildPath}
 rm -f ${ipaName}
 xcodebuild -exportArchive -exportFormat IPA -archivePath ${buildPath} -exportPath ${ipaName} -exportProvisioningProfile "${provisioning}"
