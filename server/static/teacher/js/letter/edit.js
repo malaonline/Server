0aa7732dcdbfab6e84d83a79cbf701f10f5d84f2
jackyZhang <zhangzhengliang@malalaoshi.com>
2016-04-12 11:22:34 +0800

    TWEB-27: value check

--- server/static/teacher/js/letter/edit.js
+++ server/static/teacher/js/letter/edit.js
@@ -1,12 +1,30 @@
-var isSending = false;
-function send(o){
-  if(isSending){
+var letterIsSending = false;
+function letterSend(o){
+  if(letterIsSending){
     return;
   }
-  $('.submit-action').css('display', 'block');
-  isSending = true;
-  $(o).attr('disabled', true);
+  var titleVal = $('#title').val();
+  var contentVal = $('#content').val();
+  var emsg = null;
+  if(titleVal.trim().length == 0){
+    emsg = '标题不能为空';
+  }
+  if(contentVal.trim().length == 0){
+    emsg = !emsg ? '内容不能为空' : emsg + '\r内容不能为空';
+  }
+  if(emsg){
+    alert(emsg);
+    return false;
+  }
+  letterSetSendingView(o);
 }
-function docancle(href){
+function letterDocancle(href){
   document.location.href = href;
+}
+function letterSetSendingView(o){
+  $('.submit-action').css('display', 'block');
+  letterIsSending = true;
+  $(o).attr('disabled', true);
+  $('#title').attr('disabled', true);
+  $('#content').attr('disabled', true);
 }
