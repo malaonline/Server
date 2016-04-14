5c1ff9f4fc188107627e4979c28e580295e02c2c
Mengjun Liu <liumengjun@malalaoshi.com>
2016-04-14 16:16:53 +0800

    WEC-58: add csrftoken for zepto.ajax

--- server/static/common/js/django-ajax-amend.js
+++ server/static/common/js/django-ajax-amend.js
@@ -8,17 +8,17 @@ function getCookie(name) {
     if (document.cookie && document.cookie != '') {
         var cookies = document.cookie.split(';');
         for (var i = 0; i < cookies.length; i++) {
-            var cookie = jQuery.trim(cookies[i]);
+            var cookie = $.trim(cookies[i]);
             // Does this cookie string begin with the name we want?
             if (cookie.substring(0, name.length + 1) == (name + '=')) {
                 cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
                 break;
             }
         }
-    }else{
+    } else {
         throw "can't get cookies";
     }
-    if(cookieValue == null ){
+    if (cookieValue == null) {
         cookieValue = $.cookie(name);
     }
 
@@ -28,14 +28,29 @@ function csrfSafeMethod(method) {
     // these HTTP methods do not require CSRF protection
     return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
 }
-$.ajaxSetup({
-    beforeSend: function(xhr, settings) {
-        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
-            var csrftoken = getCookie('csrftoken');
-            if (csrftoken==null){
-                throw "can't get csrftoken from cookies";
-            }
+var _beforeSend0 = function(xhr, options) {
+    if (!csrfSafeMethod(options.type) && !this.crossDomain) {
+        var csrftoken = getCookie('csrftoken');
+        if (csrftoken == null) {
+            throw "can't get csrftoken from cookies";
+        }
+        if (xhr.setRequestHeader) {
             xhr.setRequestHeader("X-CSRFToken", csrftoken);
+        } else {
+            options.data["X-CSRFToken"] = csrftoken;
         }
     }
-});
+};
+if ($.ajaxSetup) { // jQuery
+    $.ajaxSetup({
+        beforeSend: function(xhr, settings) {
+            _beforeSend0(xhr, settings);
+        }
+    });
+} else { // for zepto.js
+    $(document).on(
+        'ajaxBeforeSend', function(e, xhr, options) {
+            _beforeSend0(xhr, options);
+        }
+    );
+}
