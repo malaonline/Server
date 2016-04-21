fd8553ba7eb5992e6cd4bee83424f677553728b9
wangxinyu <wangxinyu@malalaoshi.com>
2016-04-21 11:48:35 +0800

    IOS-295 update build script

--- ios/buildbot.sh
+++ ios/buildbot.sh
@@ -2,7 +2,6 @@
 set -e
 
 scheme="parent"
-AppStoreProvisioning="com.malalaoshi.app-AppStore"
 AdHocProvisioning="com.malalaoshi.app-AdHoc"
 ipaDir="build/ipa/"
 mkdir -p ${ipaDir}
@@ -12,17 +11,24 @@ security -v unlock-keychain -p ${KEYCHAIN_PASSWORD} ${KEYCHAIN_PATH}
 security set-keychain-settings -l -u -t 3600 ${KEYCHAIN_PATH}
 security list-keychains -s ${KEYCHAIN_PATH}
 
+# UnitTest
+scheme="parentTests"
+
+# xctool should upgrade to 0.2.9 or above
+xctool -workspace mala-ios.xcworkspace -scheme ${scheme} -configuration DevRelease -sdk iphonesimulator test -freshInstall -freshSimulator
+
+
+# DevRelease
+scheme="parent"
 configuration="DevRelease"
 buildPath="build/archive/${scheme}_dev_release.xcarchive"
 ipaName="${ipaDir}${scheme}_dev_release.ipa"
-
-# xctool should upgrade to 0.2.9 or above
-#xctool -workspace mala-ios.xcworkspace -scheme ${scheme} -configuration DevRelease -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 5s,OS=8.4' test
 
 xctool -workspace mala-ios.xcworkspace -scheme ${scheme} -configuration ${configuration} archive -archivePath ${buildPath}
 xcodebuild -exportArchive -exportFormat IPA -archivePath ${buildPath} -exportPath ${ipaName} -exportProvisioningProfile "${AdHocProvisioning}"
 
 
+# PrdRelease
 configuration="PrdRelease"
 buildPath="build/archive/${scheme}_prd_release.xcarchive"
 ipaName="${ipaDir}${scheme}_prd_release.ipa"
