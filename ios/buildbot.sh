ef3d6638a4c751c548549ee3e2682a4569fd0e6e
wangxinyu <wangxinyu@malalaoshi.com>
2016-04-22 11:59:47 +0800

    IOS-300 update build script

--- ios/buildbot.sh
+++ ios/buildbot.sh
@@ -17,7 +17,6 @@ AdHocProvisioning="com.malalaoshi.app-AdHoc"
 security -v unlock-keychain -p ${KEYCHAIN_PASSWORD} ${KEYCHAIN_PATH}
 security set-keychain-settings -l -u -t 3600 ${KEYCHAIN_PATH}
 security list-keychains -s ${KEYCHAIN_PATH}
-security authorizationdb write system.privilege.taskport allow
 
 
 # Unit Test
