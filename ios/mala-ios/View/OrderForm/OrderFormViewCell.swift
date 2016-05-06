02cadab72bf6c8ca050a0897ddd37ec3c6192cb0
wangxinyu <wangxinyu@malalaoshi.com>
2016-05-06 15:43:39 +0800

    IOS-310 add a new profile elements for order form

---
+++ ios/mala-ios/View/OrderForm/OrderFormViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  OrderFormViewCell.swift
+//  mala-ios
+//
+//  Created by 王新宇 on 16/5/6.
+//  Copyright © 2016年 Mala Online. All rights reserved.
+//
+
+import UIKit
+
+class OrderFormViewCell: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
