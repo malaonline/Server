da921747cfd311dc81527c96a02ad82a8250e13d
wangxinyu <wangxinyu@malalaoshi.com>
2016-05-09 16:25:07 +0800

    IOS-300 update build script

--- ios/buildbot.sh
+++ ios/buildbot.sh
@@ -1,30 +1,41 @@
 #!/bin/sh
 set -e
 
+# Build configurations
 scheme="parent"
-provisioning="For test"
+configuration="DevRelease"
 ipaDir="build/ipa/"
+
+
+# Clean
 mkdir -p ${ipaDir}
 rm -rf ${ipaDir}*.ipa
 
+
+# Provisioning configurations
+AdHocProvisioning="com.malalaoshi.app-AdHoc"
 security -v unlock-keychain -p ${KEYCHAIN_PASSWORD} ${KEYCHAIN_PATH}
 security set-keychain-settings -l -u -t 3600 ${KEYCHAIN_PATH}
 security list-keychains -s ${KEYCHAIN_PATH}
 
-configuration="DevRelease"
+
+# Unit Test
+# xctool -workspace mala-ios.xcworkspace -scheme ${scheme} -configuration DevRelease -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 5s,OS=8.4' test -freshInstall -freshSimulator
+
+
+# Compile Project DevRelease
+scheme="parent"
 buildPath="build/archive/${scheme}_dev_release.xcarchive"
 ipaName="${ipaDir}${scheme}_dev_release.ipa"
 
-# xctool should upgrade to 0.2.9 or above
-xctool -workspace mala-ios.xcworkspace -scheme ${scheme} -configuration DevRelease -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 5s,OS=8.4' test -freshSimulator
-
 xctool -workspace mala-ios.xcworkspace -scheme ${scheme} -configuration ${configuration} archive -archivePath ${buildPath}
-xcodebuild -exportArchive -exportFormat IPA -archivePath ${buildPath} -exportPath ${ipaName} -exportProvisioningProfile "${provisioning}"
+xcodebuild -exportArchive -exportFormat IPA -archivePath ${buildPath} -exportPath ${ipaName} -exportProvisioningProfile "${AdHocProvisioning}"
 
 
+# Compile Project PrdRelease
 configuration="PrdRelease"
 buildPath="build/archive/${scheme}_prd_release.xcarchive"
 ipaName="${ipaDir}${scheme}_prd_release.ipa"
 
 xctool -workspace mala-ios.xcworkspace -scheme ${scheme} -configuration ${configuration} archive -archivePath ${buildPath}
-xcodebuild -exportArchive -exportFormat IPA -archivePath ${buildPath} -exportPath ${ipaName} -exportProvisioningProfile "${provisioning}"
+xcodebuild -exportArchive -exportFormat IPA -archivePath ${buildPath} -exportPath ${ipaName} -exportProvisioningProfile "${AdHocProvisioning}"
