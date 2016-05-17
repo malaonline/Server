a7dedd70f42e68af30ccfe222489a511b82619a1
Leon <i@leons.im>
2016-05-17 15:32:11 +0800

    AN-269 display java and javac version.

--- android/buildbot.sh
+++ android/buildbot.sh
@@ -3,7 +3,10 @@ set -e
 
 # gradle build
 cp /opt/keys-pros/*.properties ./
+
+java -version
+javac -version
+gradle --version
 # no teacher client temporarily
 gradle assembleParentDevrelease assembleParentStagerelease assembleParentRelease
-gradle --version
 gradle testParentDevreleaseUnitTest testParentStagereleaseUnitTest testParentReleaseUnitTest
