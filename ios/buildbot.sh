4d0a8110cb324d0d5b42435a64afed16740ebafd
Leon <i@leons.im>
2016-05-30 20:17:04 +0800

    IOS-329: Concurrently build 3 targets.

--- ios/buildbot.sh
+++ ios/buildbot.sh
@@ -2,18 +2,21 @@
 set -e
 
 # Build configurations
+scheme="parent"
 ipaDir="build/ipa/"
 configuration="Release"
+derivedDataPath="build/derivedDada"
 
 # Clean
 mkdir -p ${ipaDir}
 rm -rf ${ipaDir}*.ipa
+rm -rf ${derivedDataPath}
 
 # Provisioning configurations
 AdHocProvisioning="com.malalaoshi.app-AppStore"
 security -v unlock-keychain -p ${KEYCHAIN_PASSWORD} ${KEYCHAIN_PATH}
 security set-keychain-settings ${KEYCHAIN_PATH}
-security list-keychains
+security list-keychains -s ${KEYCHAIN_PATH}
 
 
 # Unit Test
@@ -21,38 +24,26 @@ security list-keychains
 
 
 # Compile Project Release
+buildPath="build/${scheme}.xcarchive"
+xctool -workspace mala-ios.xcworkspace -scheme ${scheme} -configuration ${configuration} archive -archivePath ${buildPath} -derivedDataPath ${derivedDataPath}
 
+python --version
+python replace_info.py
 
 # Export dev package
-scheme="parent-dev"
-buildPath="build/${scheme}.xcarchive"
-ipaName="${ipaDir}${scheme}_release.ipa"
-derivedDataPath="build/derivedData-dev"
-
-xctool -workspace mala-ios.xcworkspace -scheme ${scheme} -configuration ${configuration} archive -archivePath ${buildPath} -derivedDataPath ${derivedDataPath}
+cfg="dev"
+ipaName="${ipaDir}${scheme}_${cfg}_release.ipa"
+mv build/${cfg}-Info.plist build/${scheme}.xcarchive/Info.plist
 xcodebuild -exportArchive -exportFormat IPA -archivePath ${buildPath} -exportPath ${ipaName} -exportProvisioningProfile "${AdHocProvisioning}"
 
-
-if [ $1 = "dev" ]
-then
-    echo 'Only build dev for features branches.'
-    exit
-fi
-
-# Building stage package
-scheme="parent-stage"
-buildPath="build/${scheme}.xcarchive"
-ipaName="${ipaDir}${scheme}_release.ipa"
-derivedDataPath="build/derivedData-stage"
-
-xctool -workspace mala-ios.xcworkspace -scheme ${scheme} -configuration ${configuration} archive -archivePath ${buildPath} -derivedDataPath ${derivedDataPath}
+# Export stage package
+cfg="stage"
+ipaName="${ipaDir}${scheme}_${cfg}_release.ipa"
+mv build/${cfg}-Info.plist build/${scheme}.xcarchive/Info.plist
 xcodebuild -exportArchive -exportFormat IPA -archivePath ${buildPath} -exportPath ${ipaName} -exportProvisioningProfile "${AdHocProvisioning}"
 
-# Buildng prd package
-scheme="parent-prd"
-buildPath="build/${scheme}.xcarchive"
-ipaName="${ipaDir}${scheme}_release.ipa"
-derivedDataPath="build/derivedData-prd"
-
-xctool -workspace mala-ios.xcworkspace -scheme ${scheme} -configuration ${configuration} archive -archivePath ${buildPath} -derivedDataPath ${derivedDataPath}
+# Export prd package
+cfg="prd"
+ipaName="${ipaDir}${scheme}_${cfg}_release.ipa"
+mv build/${cfg}-Info.plist build/${scheme}.xcarchive/Info.plist
 xcodebuild -exportArchive -exportFormat IPA -archivePath ${buildPath} -exportPath ${ipaName} -exportProvisioningProfile "${AdHocProvisioning}"
