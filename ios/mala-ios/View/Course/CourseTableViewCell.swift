0b848d16e6aa4b2b15c834fde7378af034858d15
wangxinyu <wangxinyu@malalaoshi.com>
2016-06-14 16:18:13 +0800

    IOS-351 add image resource

---
+++ ios/mala-ios/View/Course/CourseTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CourseTableViewCell.swift
+//  mala-ios
+//
+//  Created by 王新宇 on 16/6/14.
+//  Copyright © 2016年 Mala Online. All rights reserved.
+//
+
+import UIKit
+
+class CourseTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
