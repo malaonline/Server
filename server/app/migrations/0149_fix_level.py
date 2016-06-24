fae9f3b50c1fcd0fd321e9a9d13c476461a8fb70
Mengjun Liu <liumengjun@malalaoshi.com>
2016-06-24 11:07:47 +0800

    BE-149: modify numbers

--- server/app/migrations/0149_fix_level.py
+++ server/app/migrations/0149_fix_level.py
@@ -13,21 +13,21 @@ def fix_level(apps, schema_editor):
     Region = apps.get_model('app', 'Region')
 
     chinanums = '一二三四五六七八九十'
-    commission_percentages = [10,10,10,10,10,10,10,10,10,10]
+    commission_percentages = [20,20,20,20,20,20,20,20,20,20]
+    price_cfg = [2000,3000,4000,5000,6000,7000,8000,9000,10000,11000]
     region = Region.objects.get(name='许昌市')
     region.opened = True
     region.save()
     abilities = Ability.objects.all()
 
-    print("修正教师等级")
+    # print("修正教师等级")
     for i in range(0,len(chinanums)):
         level, _ = Level.objects.get_or_create(id=i+1, defaults={'level_order': 0})
         level.name = chinanums[i]+'级'
         level.save()
         #print(" {level_name}".format(level_name=level.name))
-        p = 100 + i * 30
         for ability in abilities:
-            c = p + random.randint(0, 10) * 10
+            c = price_cfg[i]
             price, _ = Price.objects.get_or_create(region=region, level=level, ability=ability,
                                                    defaults={'price': c})
             price.price = c
