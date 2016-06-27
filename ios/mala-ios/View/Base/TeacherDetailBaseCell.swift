457348135cb911dc3fc4cd4f657baea4718c462f
wangxinyu <wangxinyu@malalaoshi.com>
2016-06-27 18:29:43 +0800

    IOS-379 remove useless file

---
+++ ios/mala-ios/View/Base/TeacherDetailBaseCell.swift
@@ -0,0 +1,21 @@
+//
+//  TeacherDetailBaseCell.swift
+//  mala-ios
+//
+//  Created by 王新宇 on 16/6/27.
+//  Copyright © 2016年 Mala Online. All rights reserved.
+//
+
+import UIKit
+
+class TeacherDetailBaseCell: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
