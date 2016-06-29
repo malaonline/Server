2250782771abace9851b9a89a86cd5ae304eb82b
wangxinyu <wangxinyu@malalaoshi.com>
2016-06-29 18:34:30 +0800

    IOS-349 update JPush

--- ios/mala-ios/Common/MalaRemoteNotificationHandler.swift
+++ ios/mala-ios/Common/MalaRemoteNotificationHandler.swift
@@ -14,25 +14,31 @@ public class MalaRemoteNotificationHandler: NSObject {
     // MARK: - Property
     
     /// 通知类型键
-    public let kNotificationType = "notificationtype"
+    public let kNotificationType = "type"
+    /// 附带参数键（暂时仅当type为2时，附带code为订单号）
+    public let kNotificationCode = "code"
     
     ///  通知类型
     ///
-    ///  - Changed:  调课
-    ///  - Stoped:   停课
-    ///  - Finished: 完课
-    ///  - Starting: 开课
+    ///  - Changed:     调课完成 -> 课表
+    ///  - Stoped:      退费成功 -> 订单详情
+    ///  - Finished:    课程结束 -> 我的评价
+    ///  - Starting:    上课通知 -> 课表
+    ///  - Maturity:    奖学金到期 -> 我的奖学金
+    ///  - Evaluation:  测评建档 -> 课表
     public enum RemoteNotificationType: Int {
         case Changed = 1
-        case Stoped = 2
+        case Refunds = 2
         case Finished = 3
         case Starting = 4
+        case Maturity = 5
+        case Evaluation = 6
     }
     
     /// 远程推送通知处理对象
     private var remoteNotificationTypeHandler: RemoteNotificationType? {
         willSet {
-            
+            println("远程推送通知处理对象 - \(newValue)")
             if let
                 type = newValue,
                 appDelegate = UIApplication.sharedApplication().delegate as? AppDelegate,
@@ -41,26 +47,71 @@ public class MalaRemoteNotificationHandler: NSObject {
                 
                 // 提示信息
                 var title: String = ""
-                // 提示框确认闭包
-                let action: () -> () = {
-                    appDelegate.switchTabBarControllerWithIndex(1)
-                }
+                // 提示确认闭包
+                var action: ()->() = {}
                 
                 ///  匹配信息类型
                 switch type {
                     
                 case .Changed:
-                    title = "调课完成"
-            
-                case .Stoped:
-                    title = "停课完成"
+                    title = "课程变动"
+                    action = {
+                        /// 课表页
+                        appDelegate.switchTabBarControllerWithIndex(1)
+                    }
+                    
+                case .Refunds:
+                    title = "退费成功"
+                    action = {
+                        /// 订单详情页
+                        if let viewController = getActivityViewController() where self.code != 0 {
+                            
+                            println("退费成功 - \(viewController) - \(self.code)")
+                            
+                            if let mainViewController = viewController as? MainViewController,
+                                naviVC = mainViewController.viewControllers?[0] as? UINavigationController {
+                                let orderFormViewController = OrderFormInfoViewController()
+                                orderFormViewController.id = self.code
+                                naviVC.pushViewController(orderFormViewController, animated: true)
+                            }else {
+                                let orderFormViewController = OrderFormInfoViewController()
+                                orderFormViewController.id = self.code
+                                viewController.navigationController?.pushViewController(orderFormViewController, animated: true)
+                            }
+                        }
+                    }
             
                 case .Finished:
                     title = "完课评价"
-            
+                    action = {
+                        /// 我的评价
+                        if let viewController = getActivityViewController() {
+                            viewController.navigationController?.pushViewController(CommentViewController(), animated: true)
+                        }
+                    }
+                    
                 case .Starting:
                     title = "课前通知"
-            
+                    action = {
+                        /// 课表页
+                        appDelegate.switchTabBarControllerWithIndex(1)
+                    }
+                    
+                case .Maturity:
+                    title = "奖学金即将到期"
+                    action = {
+                        /// 我的奖学金
+                        if let viewController = getActivityViewController() {
+                            viewController.navigationController?.pushViewController(CouponViewController(), animated: true)
+                        }
+                    }
+                    
+                case .Evaluation:
+                    title = "测评建档"
+                    action = {
+                        /// 课表页
+                        appDelegate.switchTabBarControllerWithIndex(1)
+                    }
                 }
                 
                 // 若当前在前台，弹出提示
@@ -72,19 +123,25 @@ public class MalaRemoteNotificationHandler: NSObject {
                         MalaAlert.confirmOrCancel(
                             title: title,
                             message: message,
-                            confirmTitle: "前往课表",
-                            cancelTitle: "取消",
+                            confirmTitle: "去查看",
+                            cancelTitle: "知道了",
                             inViewController: viewController,
-                            withConfirmAction: action, cancelAction: { () -> Void in
-                        })
+                            withConfirmAction: action, cancelAction: {})
                     }
                 }
+                
             }
         }
     }
     
     /// 通知信息字典
     private var notificationInfo: [NSObject : AnyObject] = [NSObject : AnyObject]()
+    /// 附带参数（订单号）
+    private var code: Int = 0 {
+        didSet {
+            println("code - \(code)")
+        }
+    }
     
     
     // MARK: - Method
@@ -92,14 +149,17 @@ public class MalaRemoteNotificationHandler: NSObject {
     ///
     ///  - parameter userInfo: 通知信息字典
     public func handleRemoteNotification(userInfo: [NSObject : AnyObject]) -> Bool {
-        
         if let
             type = Int(userInfo[kNotificationType] as? String ?? "0"),
             remoteNotificationType = RemoteNotificationType(rawValue: type) {
             
+            if let code = Int(userInfo[kNotificationCode] as? String ?? "0") {
+                self.code = code
+            }
+            
             notificationInfo = userInfo
             remoteNotificationTypeHandler = remoteNotificationType
-                
+            
             return true
         }else {
             return false
