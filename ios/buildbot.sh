7121d9e2e57f2ed8bc72ef70d74a3d958c4a9bac
wangxinyu <wangxinyu@malalaoshi.com>
2016-07-01 12:28:49 +0800

    IOS-178 update build script for new provisioning profile

--- ios/buildbot.sh
+++ ios/buildbot.sh
@@ -13,7 +13,6 @@ rm -rf ${ipaDir}*.ipa
 rm -rf ${derivedDataPath}
 
 # Provisioning configurations
-AdHocProvisioning="Mala_DevAdHoc"
 security -v unlock-keychain -p ${KEYCHAIN_PASSWORD} ${KEYCHAIN_PATH}
 security set-keychain-settings ${KEYCHAIN_PATH}
 security list-keychains -s ${KEYCHAIN_PATH}
@@ -32,19 +31,21 @@ python replace_info.py
 
 # Export dev package
 cfg="dev"
+AdHocProvisioning="Mala_Dev-AdHoc"
 ipaName="${ipaDir}${scheme}_${cfg}_release.ipa"
 mv build/${cfg}-Info.plist build/${scheme}.xcarchive/Info.plist
 xcodebuild -exportArchive -exportFormat IPA -archivePath ${buildPath} -exportPath ${ipaName} -exportProvisioningProfile "${AdHocProvisioning}"
 
 # Export stage package
 cfg="stage"
+AdHocProvisioning="Mala_Stage-AdHoc"
 ipaName="${ipaDir}${scheme}_${cfg}_release.ipa"
 mv build/${cfg}-Info.plist build/${scheme}.xcarchive/Info.plist
 xcodebuild -exportArchive -exportFormat IPA -archivePath ${buildPath} -exportPath ${ipaName} -exportProvisioningProfile "${AdHocProvisioning}"
 
 # Export prd package
 cfg="prd"
-AdHocProvisioning="Mala_PrdAdHoc"
+AdHocProvisioning="Mala_Prd-AdHoc"
 ipaName="${ipaDir}${scheme}_${cfg}_release.ipa"
 mv build/${cfg}-Info.plist build/${scheme}.xcarchive/Info.plist
 xcodebuild -exportArchive -exportFormat IPA -archivePath ${buildPath} -exportPath ${ipaName} -exportProvisioningProfile "${AdHocProvisioning}"
