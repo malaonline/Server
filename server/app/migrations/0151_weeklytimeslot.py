c6fb3c20c44467867dae39252d055edf4bce25a8
Shang Erdi <shangerdi@malalaoshi.com>
2016-07-12 11:45:06 +0800

    SERVER-228: Modified Weekly Time Slots for Zheng Zhou

---
+++ server/app/migrations/0151_weeklytimeslot.py
@@ -0,0 +1,32 @@
+import os
+from datetime import time
+
+from django.db import migrations
+
+
+def edit_weekly_time_slot(apps, schema_editor):
+    WeeklyTimeSlot = apps.get_model('app', 'WeeklyTimeSlot')
+
+    weekly_time_slots = list(
+        WeeklyTimeSlot.objects.all().order_by('weekday', 'start')
+    )
+    index = 0
+
+    for weekday in range(1, 8):
+        for start in ((8, 0), (10, 30), (14, 00), (16, 30), (19, 0)):
+            end = (start[0] + 2, start[1])
+            one_weekly_time_slot = weekly_time_slots[index]
+            one_weekly_time_slot.start = time(*start)
+            one_weekly_time_slot.end = time(*end)
+            one_weekly_time_slot.save()
+            index += 1
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('app', '0150_evaluation_reminded'),
+    ]
+
+    operations = [
+        migrations.RunPython(edit_weekly_time_slot),
+    ]
