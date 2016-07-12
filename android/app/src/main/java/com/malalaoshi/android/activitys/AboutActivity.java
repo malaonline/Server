073040746e56723cb961fdac5668883c07a82ef6
Kang <1123180416@qq.com>
2016-07-12 14:13:44 +0800

    AN-343 modify the app version code and version name

--- android/app/src/main/java/com/malalaoshi/android/activitys/AboutActivity.java
+++ android/app/src/main/java/com/malalaoshi/android/activitys/AboutActivity.java
@@ -1,6 +1,9 @@
 package com.malalaoshi.android.activitys;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.malalaoshi.android.R;
 import com.malalaoshi.android.core.base.BaseActivity;
@@ -15,12 +18,23 @@ import butterknife.ButterKnife;
 public class AboutActivity extends BaseActivity implements TitleBarView.OnTitleBarClickListener {
     @Bind(R.id.titleBar)
     TitleBarView titleBar;
+
+    @Bind(R.id.tv_version_name)
+    TextView tvVersionName;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
         ButterKnife.bind(this);
         titleBar.setOnTitleBarClickListener(this);
+        PackageManager pm = getPackageManager();
+        try {
+            PackageInfo pi = pm.getPackageInfo(getPackageName(),0);
+            tvVersionName.setText("版本 V"+pi.versionName);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
