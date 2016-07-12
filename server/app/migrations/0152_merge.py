abc9dc3776d0f190e00ae099cf912cc26eccb992
Shang Erdi <shangerdi@malalaoshi.com>
2016-07-12 17:18:14 +0800

    SERVER-228: Modified Weekly Time Slots for Zheng Zhou

---
+++ server/app/migrations/0152_merge.py
@@ -0,0 +1,9 @@
+from django.db import migrations
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('app', '0059_weeklytimeslot'),
+        ('app', '0151_set_tenth_level_price'),
+    ]
+
+    operations = []
