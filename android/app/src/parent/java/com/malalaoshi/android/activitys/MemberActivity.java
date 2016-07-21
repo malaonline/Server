01114b11e32be91c03afd1dc1cdef0a28d561293
Kang <1123180416@qq.com>
2016-07-21 10:44:24 +0800

    AN-358 update app version

--- android/app/src/parent/java/com/malalaoshi/android/activitys/MemberActivity.java
+++ android/app/src/parent/java/com/malalaoshi/android/activitys/MemberActivity.java
@@ -4,15 +4,9 @@ package com.malalaoshi.android.activitys;
 import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.MotionEvent;
-import android.view.View;
-
-import com.malalaoshi.android.MainActivity;
 import com.malalaoshi.android.R;
 import com.malalaoshi.android.core.base.BaseActivity;
 import com.malalaoshi.android.core.view.TitleBarView;
