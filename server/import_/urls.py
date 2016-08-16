a67f9ab6a25876b66437eaaa6207a01b952974f5
Leon <i@leons.im>
2016-08-16 17:04:40 +0800

    SERVER-242: Add templates for importing data.

--- server/import_/urls.py
+++ server/import_/urls.py
@@ -4,6 +4,11 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name="index"),
-    url(r'^login/$', views.login, name="login"),
-    url(r'^logout/$', views.logout, name="logout"),
+
+    url(r'^login$', views.LoginView.as_view(), name="login"),
+    url(r'^logout$', views.logout, name="logout"),
+
+    url(r'^teachers$', views.TeacherView.as_view(), name="teachers"),
+    url(r'^parents$', views.ParentView.as_view(), name="parents"),
+    url(r'^orders$', views.OrderView.as_view(), name="orders"),
 ]
--- server/import_/views.py
+++ server/import_/views.py
@@ -1,34 +1,15 @@
 import logging
-import datetime
-import json
-from collections import OrderedDict
-import xlwt
 
 # django modules
-from django.conf import settings
-from django.core.files.base import ContentFile
-from django.shortcuts import render, redirect, get_object_or_404
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.http import require_POST
-from django.views.generic import View, TemplateView, ListView
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.views.generic import View, TemplateView
 from django.utils.decorators import method_decorator
 from django.contrib import auth
-from django.db import IntegrityError, transaction
-from django.db.models import Q
-from django.utils import timezone
-from rest_framework.renderers import JSONRenderer
 from django.contrib.auth.mixins import AccessMixin
 
 # local modules
-from app import models
-from app.utils import smsUtil
-from app.utils.algorithm import check_id_number
-from app.utils.types import parseInt, parse_date, parse_date_next
-from app.utils.db import paginate, Pager
-from app.utils import excel
 from .decorators import mala_staff_required, is_manager
-from app.exception import TimeSlotConflict, OrderStatusIncorrect, RefundError
-from app.tasks import send_push, Remind, send_sms
 
 
 logger = logging.getLogger('app')
@@ -36,13 +17,33 @@ logger = logging.getLogger('app')
 
 @mala_staff_required
 def index(request):
-    return render(request, 'import_/index.html')
+    return redirect('import_:teachers')
 
+class LoginView(View):
+    def get(self, request):
+        if is_manager(request.user):
+            return redirect('import_:index')
+        return render(request, 'import_/login.html')
 
-def login(request, context={}):
-    if is_manager(request.user):
-        return redirect('import_:index')
-    return render(request, 'import_/login.html', context)
+    def post(self, request):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        goto_page = request.POST.get('next')
+        logger.debug('try to login, username: '+username+', password: '+password+', goto_page: '+str(goto_page))
+        # TODO: 错误信息包含‘错误码’，错误描述可能会变
+        if not username or not password:
+            return login(request, {'errors': '请输入用户名和密码'})
+        # 登录前需要先验证
+        newUser=auth.authenticate(username=username,password=password)
+        if newUser is not None:
+            if not is_manager(newUser):
+                return login(request, {'errors': '你不是管理员呀'})
+            auth.login(request, newUser)
+            if goto_page:
+                return redirect(goto_page)
+            else:
+                return redirect('import_:index')
+        return login(request, {'errors': '用户名或密码错误'})
 
 
 def logout(request):
@@ -57,7 +58,8 @@ class StaffRoleRequiredMixin(AccessMixin):
             for staff_permission in group.staffpermission_set.all():
                 if staff_permission.allowed_url_name == 'all' \
                         or staff_permission.allowed_url_name == url_name:
-                    return super(StaffRoleRequiredMixin, self).dispatch(request, *args, **kwargs)
+                    return super(StaffRoleRequiredMixin, self).dispatch(
+                            request, *args, **kwargs)
 
         return HttpResponse("Not Allowed.", status=403)
 
@@ -82,4 +84,17 @@ class BaseStaffActionView(StaffRoleRequiredMixin, View):
     # @method_decorator(csrf_exempt) # 不加csrf,不允许跨域访问
     @method_decorator(mala_staff_required)
     def dispatch(self, request, *args, **kwargs):
-        return super(BaseStaffActionView, self).dispatch(request, *args, **kwargs)
+        return super(BaseStaffActionView, self).dispatch(
+                request, *args, **kwargs)
+
+
+class TeacherView(BaseStaffView):
+    template_name = 'import_/teachers.html'
+
+
+class ParentView(BaseStaffView):
+    template_name = 'import_/parents.html'
+
+
+class OrderView(BaseStaffView):
+    template_name = 'import_/orders.html'
