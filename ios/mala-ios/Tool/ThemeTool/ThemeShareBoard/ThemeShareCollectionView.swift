2745af604fb363e5457864d5a1849d6199e33bc5
wangxinyu <wangxinyu@malalaoshi.com>
2016-08-23 17:21:51 +0800

    IOS-438 add ThemeShareBoard and image resource

---
+++ ios/mala-ios/Tool/ThemeTool/ThemeShareBoard/ThemeShareCollectionView.swift
@@ -0,0 +1,21 @@
+//
+//  ThemeShareCollectionView.swift
+//  mala-ios
+//
+//  Created by 王新宇 on 16/8/23.
+//  Copyright © 2016年 Mala Online. All rights reserved.
+//
+
+import UIKit
+
+class ThemeShareCollectionView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
