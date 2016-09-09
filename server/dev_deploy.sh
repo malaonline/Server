1180969f9b4ea498b999e722d60764b4aa00c2a0
Mengjun Liu <mengjun_liu@126.com>
2016-09-09 17:02:12 +0800

    SERVER-259: fix dev deploy cfg

--- server/dev_deploy.sh
+++ server/dev_deploy.sh
@@ -1,1 +1,1 @@
-ssh -i dev_deploy.key ec2-user@dev.malalaoshi.com /home/ec2-user/bin/dev_deploy.sh
+ssh -i dev_deploy.key -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ec2-user@dev.malalaoshi.com /home/ec2-user/bin/dev_deploy.sh
