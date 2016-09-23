58b2e0da3ba9b76d52251e6a7b437aa0927fccfc
Shang Erdi <shangerdi@malalaoshi.com>
2016-09-23 16:20:33 +0800

    BE-215: Added school prices init method

--- server/app/models.py
+++ server/app/models.py
@@ -165,6 +165,47 @@ class School(BaseModel):
     def get_member_service_list(self):
         return self.member_services.all().values_list('id', flat=True) or []
 
+    def init_prices(self):
+        if not self.priceconfig_set.exists():
+            # using yuan for better look
+            configs = [
+                ('一年级', [170, 165, 160, 150]),
+                ('二年级', [170, 165, 160, 150]),
+                ('三年级', [180, 175, 170, 160]),
+                ('四年级', [180, 175, 170, 160]),
+                ('五年级', [180, 175, 170, 160]),
+                ('六年级', [200, 195, 190, 180]),
+                ('初一', [200, 195, 190, 180]),
+                ('初二', [210, 200, 190, 180]),
+                ('初三', [230, 220, 200, 190]),
+                ('高一', [240, 235, 230, 220]),
+                ('高二', [255, 250, 240, 235]),
+                ('高三', [170, 165, 160, 150]),
+            ]
+            hours_ranges = [(1, 10), (11, 20), (21, 50), (51, 100)]
+            for level_id in range(1, 10+1):
+                # each level's price delta, 10 yuan
+                price_delta = (level_id-1)*10
+                level = Level.objects.get(pk=level_id)
+                for config in configs:
+                    grade_name = config[0]
+                    grade = Grade.objects.get(name=grade_name)
+                    prices = config[1]
+                    for index, hours_range in enumerate(hours_ranges):
+                        min_hours = hours_range[0]
+                        max_hours = hours_range[1]
+                        # real price is in fen unit
+                        price = (prices[index] + price_delta)*100
+                        price_config, created =\
+                            PriceConfig.objects.get_or_create(
+                                school=self,
+                                level=level,
+                                grade=grade,
+                                min_hours=min_hours,
+                                max_hours=max_hours,
+                                price=price,
+                            )
+
 
 class SchoolPhoto(BaseModel):
     school = models.ForeignKey(School)
