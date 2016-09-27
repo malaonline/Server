46f898278f6261cdf50e8ab22f4203aa4b9fee7c
Mengjun Liu <mengjun_liu@126.com>
2016-09-27 15:28:16 +0800

    BE-218: simplify code

--- server/static/staff/js/school_account/income_audit.js
+++ server/static/staff/js/school_account/income_audit.js
@@ -9,38 +9,23 @@ $(function(){
         var params = {'rid': recordId};
         if (status != 'yes') {
             params['action'] = 'mark_yes';
-            malaAjaxPost(location.pathname, params, function(result){
-                if (result) {
-                    if (result.ok) {
-                        alert("操作成功");
-                        location.reload();
-                    } else {
-                        alert(result.msg);
-                    }
-                    return;
-                }
-                alert(DEFAULT_ERR_MSG);
-            }, 'json', function(jqXHR, errorType, errorDesc){
-                var errMsg = errorDesc ? ('[' + errorDesc + '] ') : '';
-                alert(errMsg + DEFAULT_ERR_MSG);
-            });
         } else {
             params['action'] = 'mark_no';
-            malaAjaxPost(location.pathname, params, function(result){
-                if (result) {
-                    if (result.ok) {
-                        alert("操作成功");
-                        location.reload();
-                    } else {
-                        alert(result.msg);
-                    }
-                    return;
-                }
-                alert(DEFAULT_ERR_MSG);
-            }, 'json', function(jqXHR, errorType, errorDesc){
-                var errMsg = errorDesc ? ('[' + errorDesc + '] ') : '';
-                alert(errMsg + DEFAULT_ERR_MSG);
-            });
         }
+        malaAjaxPost(location.pathname, params, function(result){
+            if (result) {
+                if (result.ok) {
+                    alert("操作成功");
+                    location.reload();
+                } else {
+                    alert(result.msg);
+                }
+                return;
+            }
+            alert(DEFAULT_ERR_MSG);
+        }, 'json', function(jqXHR, errorType, errorDesc){
+            var errMsg = errorDesc ? ('[' + errorDesc + '] ') : '';
+            alert(errMsg + DEFAULT_ERR_MSG);
+        });
     });
 });
