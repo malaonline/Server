f549d323869430ecb72c3b22deb0994d86f2eb16
Leon <i@leons.im>
2016-09-30 17:10:58 +0800

    Fix travis ci settings.

--- server/server/travis_ci_settings.py
+++ server/server/travis_ci_settings.py
@@ -1,6 +1,8 @@
-from ..settings import *
+from .settings import *
 
-# Jenkins单元测试专用的settings
+SECRET_KEY = 'use your secret key'
+
+# DB settings for Travis CI
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -18,14 +20,6 @@ DATABASES = {
 
 # 关闭SMS短信发送功能
 FAKE_SMS_SERVER = True
-
-# 用sqlite替代postgresql
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 
 # 把上传路径改为本地
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
