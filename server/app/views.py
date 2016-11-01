fce2cf110cc3cd1e156feb73d3ca4210fe510cb1
Shang Erdi <shangerdi@malalaoshi.com>
2016-11-01 11:30:55 +0800

    SERVER-287: Added order create failed code (-3)

--- server/app/views.py
+++ server/app/views.py
@@ -1083,6 +1083,8 @@ class OrderViewSet(ParentBasedMixin,
         live_class = request.data.get('live_class', None)
         # Do not validate coupon for live class for now
         if live_class:
+            live_class = get_object_or_404(
+                models.LiveClass, pk=request.data.get('live_class'))
             if live_class.is_full():
                 return -3  # class room is full!
             return 0
