8b55a305e9c42d711902d845c882086431f7b520
Elors <ElorsAt@iCloud.com>
2016-12-29 11:39:19 +0800

    LLWEB-5: add element-ui

--- server/static/lecturer/js/store/store_base.js
+++ server/static/lecturer/js/store/store_base.js
@@ -2,6 +2,14 @@
  * Created by Elors on 2016/12/29.
  */
 
-$(function(){
+$(function () {
+
+  Vue.component('store-container', {
+    template: '<div> This is Exercises Store Container</div>'
+  });
+
+  let root = new Vue({
+    el: '#store-root'
+  });
 
 });
